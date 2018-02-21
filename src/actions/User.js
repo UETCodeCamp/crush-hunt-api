bfbb3577d0c7690aa24c22e972e644dc3070c887
Max <tutv95@gmail.com>
2018-02-21 17:56:04 +0700

    Updated api users

--- src/actions/User.js
+++ src/actions/User.js
@@ -1,1 +1,56 @@
 const User = require('../models/User');
+const Post = require('../models/Post');
+const Comment = require('../models/Comment');
+const Favorite = require('../models/Favorite');
+const VotePost = require('../models/VotePost');
+
+exports.getProfile = (userId) => {
+    return User.findById(userId);
+};
+
+exports.getMyPosts = ({userId, page, limit}) => {
+    const skip = (page - 1) * limit;
+
+    return Post
+        .find({
+            owner: userId
+        })
+        .skip(skip)
+        .limit(limit);
+};
+
+exports.getMyVotes = ({userId, page, limit}) => {
+    const skip = (page - 1) * limit;
+
+    return VotePost
+        .find({
+            owner: userId
+        })
+        .populate('post')
+        .skip(skip)
+        .limit(limit);
+};
+
+exports.getMyComments = ({userId, page, limit}) => {
+    const skip = (page - 1) * limit;
+
+    return Comment
+        .find({
+            owner: userId
+        })
+        .populate('post')
+        .skip(skip)
+        .limit(limit);
+};
+
+exports.getMyFavorites = ({userId, page, limit}) => {
+    const skip = (page - 1) * limit;
+
+    return Favorite
+        .find({
+            owner: userId
+        })
+        .populate('post')
+        .skip(skip)
+        .limit(limit);
+};
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,19 +1,90 @@
-exports.profile = (req, res) => {
+const UserActions = require('../actions/User');
+const {sendSuccess, catchError} = require('../helpers/response');
 
+exports.profile = (req, res) => {
+    const {userId} = req;
+
+    UserActions.getProfile(userId)
+        .then(sendSuccess(req, res))
+        .catch(catchError(req, res));
 };
 
 exports.myPosts = (req, res) => {
+    const {userId} = req;
 
+    const defaultArgs = {
+        page: 1,
+        limit: 10
+    };
+
+    const {page, limit} = Object.assign({}, defaultArgs, req.query);
+
+    UserActions
+        .getMyPosts({
+            userId,
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        })
+        .then(sendSuccess(req, res))
+        .catch(catchError(req, res));
 };
 
 exports.myVotes = (req, res) => {
+    const {userId} = req;
 
+    const defaultArgs = {
+        page: 1,
+        limit: 10
+    };
+
+    const {page, limit} = Object.assign({}, defaultArgs, req.query);
+
+    UserActions
+        .getMyPosts({
+            userId,
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        })
+        .then(sendSuccess(req, res))
+        .catch(catchError(req, res));
 };
 
 exports.myComments = (req, res) => {
+    const {userId} = req;
 
+    const defaultArgs = {
+        page: 1,
+        limit: 10
+    };
+
+    const {page, limit} = Object.assign({}, defaultArgs, req.query);
+
+    UserActions
+        .getMyComments({
+            userId,
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        })
+        .then(sendSuccess(req, res))
+        .catch(catchError(req, res));
 };
 
 exports.myFavorites = (req, res) => {
+    const {userId} = req;
 
+    const defaultArgs = {
+        page: 1,
+        limit: 10
+    };
+
+    const {page, limit} = Object.assign({}, defaultArgs, req.query);
+
+    UserActions
+        .getMyFavorites({
+            userId,
+            page: parseInt(page, 10),
+            limit: parseInt(limit, 10)
+        })
+        .then(sendSuccess(req, res))
+        .catch(catchError(req, res));
 };
